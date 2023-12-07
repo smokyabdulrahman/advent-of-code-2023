a1f64ba29a882c5e74289ab2c270937ca7c810b1
aalrahma <abdulrahman.alrahma@leantech.me>
2023-12-08 00:39:24 +0300

    solved day 1

--- day1/index.js
+++ day1/index.js
@@ -1,10 +1,62 @@
+const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
-
-function crackCalibrationDocument(documentContent) {
-	return documentContent.split(
+const numbers = {
+	'one': 1,
+	'two': 2,
+	'three': 3,
+	'four': 4,
+	'five': 5,
+	'six': 6,
+	'seven': 7,
+	'eight': 8,
+	'nine': 9
 }
 
+function crackCalibrationDocument(documentContent) {
+	return documentContent.split('\n')
+	.filter(line => line.length >= 1)
+	.map(line => extractNumberFromLine(line))
+	.reduce((total, current) => total + current, 0);
+}
 
+function extractNumberFromLine(line) {
+	let i = 0,
+	    j = line.length - 1;
+	let firstNumber;
+	let lastNumber;
 
-console.log(crackCalibrationDocument(fs.readSync(path.join(__dirname, "./input.txt"))));
+	console.log("line: ", line);
+	while (i <= j && ( !firstNumber || !lastNumber )) {
+		firstNumber = getNumberIfAny(line, i);
+		lastNumber = getNumberIfAny(line, j);
+		console.log("f: ", firstNumber);
+		console.log("l: ", lastNumber);
+		if (!firstNumber) i++;
+		if (!lastNumber) j--;
+	}
+	
+	return parseInt(`${firstNumber}${lastNumber}`);
+}
+
+function getNumberIfAny(line, startIndex) {
+	return isNumber(line.charAt(startIndex))
+	? line.charAt(startIndex)
+	: getNumberFromLetteralIfAny(line, startIndex);
+}
+
+function isNumber(c) {
+  return c >= '0' && c <= '9';
+}
+
+function getNumberFromLetteralIfAny(line, startIndex) {
+	"".substring
+	return Object.keys(numbers)
+	.filter(key => key.length + startIndex <= line.length)
+	.map(key => numbers[line.substring(startIndex, key.length + startIndex)])
+	.find(num => num)
+}
+
+const filePath = path.join(__dirname, "./input.txt");
+const fileContent = fs.readFileSync(filePath, 'utf8');
+console.log(crackCalibrationDocument(fileContent));
